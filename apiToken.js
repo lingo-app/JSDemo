e5a3fdd5b95cc9d8f702b6b4e686337f6186644e
Wes Byrne <wesburn@me.com>
2021-03-11 15:28:25 -0800

    Add comment to apiToken file and remove package-log

--- apiToken.js
+++ apiToken.js
@@ -1,5 +1,9 @@
+// Add your space ID and api token here
+// This file is included in .gitignore to help ensure your credentials are not shared
+// DO NOT SHARE YOUR API TOKEN
 const spaceId = 0
 const apiToken = "<apitoken>"
+
 
 module.exports = {
     spaceId,
