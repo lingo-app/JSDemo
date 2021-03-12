61776cd6ae38f30d6684c4454ebe3de14ffc6542
Wes Byrne <wesburn@me.com>
2021-03-12 13:16:51 -0800

    copy a base apitoken file for config

---
+++ .apiToken.base.js
@@ -0,0 +1,11 @@
+// Add your space ID and api token here
+// This file is included in .gitignore to help ensure your credentials are not shared
+// DO NOT SHARE YOUR API TOKEN
+const spaceId = 0
+const apiToken = "<apitoken>"
+
+
+module.exports = {
+    spaceId,
+    apiToken
+}
