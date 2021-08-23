44cc1b74cbbe3b60ca80d79d769f5035e5c67364
Wes Byrne <wesburn@me.com>
2021-08-23 08:25:57 -0700

    Update package version and minor code example updates

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ const auth = require("./apiToken")
 // Add your spaceId and API token
 lingo.setup(auth.spaceId, auth.apiToken)
 
-lingo.baseURL = "http://local.lingoapp.com:9000/1";
-
 // Wrap the script in a function so we can use async/await
 async function runScript() {
 
     const kits = await lingo.fetchKits()
     console.log(`Your space has ${kits.length} kits`)
-    console.log(kits.map(k => `${k.name} - ${k.short_id}`))
+
 
     if (kits.length > 0) {
+        // Print the names of all kits
+        console.log(kits.map(k => `${k.short_id} - ${k.name}`))
+
         const kitId = kits[0].kit_uuid
         const kit = await lingo.fetchKit(kitId)
         const kitOutline = await lingo.fetchKitOutline(kitId)
         console.log(kit)
-        console.log(kitOutline)
+        console.log(kitOutline.sections)
     } else {
         console.log("You don't have any kits. You can create one here using")
+
+        // Creating a kit
         // WARNING: Any content created here will be available to other members of your space
         // as it would be if you created it in the Lingo app.
 
