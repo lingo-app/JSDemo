0db0b3fe8174a419114fe3210f8a039ccaa18a49
Wes Byrne <wesburn@me.com>
2026-02-23 11:56:07 -0800

    Add download example and upgrade to 3.1

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import lingo, { AssetType, ItemType, LingoError } from "@lingo-app/node";
+import lingo, { Asset, AssetType, ItemType, LingoError } from "@lingo-app/node";
 
 import auth from "./apiToken"
 // Add your spaceId and API token
@@ -22,7 +22,7 @@ async function runScript() {
         console.log(kit)
         console.log(kitOutline.sections)
     } else {
-        console.log("You don't have any kits. You can create one here using")
+        console.log("You don't have any kits. Check the docs to learn how to create  one.")
 
         // Creating a kit
         // WARNING: Any content created here will be available to other members of your space
@@ -34,6 +34,12 @@ async function runScript() {
     // Search
     let results = await lingo.search().limit(5).ofType(AssetType.SVG).fetch()
     console.log(`There are ${results.total} SVGs in your space`)
+
+    if (results.results.length > 0) {
+        const asset = results.results[0].object as Asset
+        const fileData = await lingo.downloadAsset(asset.assetUuid)
+        console.log(`Downloaded file - ${fileData.size} bytes`)
+    }
 }
 
 runScript()
